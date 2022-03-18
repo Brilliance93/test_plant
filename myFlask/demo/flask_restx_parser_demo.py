e8d1ee77b908b84893e420c1ce2f84a9119385dc
Brilliance93 <zhu_guanghuan@163.com>
2022-03-18 11:08:59 +0800

    2022003181108flask_restx_parser

--- myFlask/demo/flask_restx_parser_demo.py
+++ myFlask/demo/flask_restx_parser_demo.py
@@ -1,18 +1,41 @@
-from flask import Flask
-from flask_restx import Api,Namespace,Resource
+from flask import Flask, request
+from flask_restx import Api, Namespace, Resource, fields
+from werkzeug.datastructures import FileStorage
+
+from demo.log_util import logger
 
 app = Flask(__name__)
 api = Api(app)
 name_ns = Namespace('parse demo', description='By myself')
 
 
+@name_ns.route('')
 class ParseDemo(Resource):
     get_parser = api.parser()
-    get_parser.add_parser()
+    get_parser.add_argument('param', type=str, location='args')
+    get_parser.add_argument('age', type=int, location='args')
+    get_parser.add_argument('choice', choices=('girls', 'boys'), location='args')
+    get_parser.add_argument('file', type=FileStorage, location='files')
+    get_parser.add_argument('password', type=str, help='this is password', location='form', required=True)
+    # json 不可以和其他类型一起出现
+    # get_parser.add_argument('username', type=str, help='this is username', location='json', required=True)
 
     @name_ns.expect(get_parser)
     def get(self):
+        logger.info(f"request.args=====>{request.args}")
         return {"code": 0, "msg": "success"}
+
+    post_parser = api.parser()
+    post_parser.add_argument('username', type=str, help='this is username', location='json', required=True)
+    post_parser.add_argument('password', type=int, help='this is password', location='json', required=True)
+
+    @name_ns.expect(post_parser)
+    def post(self):
+        logger.info(f"request.json===>{request.json}")
+        return {"code": 0, "msg": "success"}
+
+
+api.add_namespace(name_ns, '/login')
 
 
 if __name__ == '__main__':
